700a473baa6831afa010afa7a67ef9eea3701706
Dharmendra Singh <javaprofessional15@gmail.com>
2018-07-24 00:36:43 +0530

    customHashmap

---
+++ codebase/Java-concepts/src/main/java/concept/java/basic/cutom/hashmap/HashMapCustomApp.java
@@ -0,0 +1,33 @@
+package concept.java.basic.cutom.hashmap;
+
+public class HashMapCustomApp {
+
+	public static void main(String[] args) {
+		HashMapCustom<Integer, Integer> hashMapCustom = new HashMapCustom<Integer, Integer>();
+		hashMapCustom.put(21, 12);
+		hashMapCustom.put(25, 121);
+		hashMapCustom.put(30, 151);
+		hashMapCustom.put(33, 15);
+		hashMapCustom.put(35, 89);
+
+		HashMapCustom<String, Integer> hashmap = new HashMapCustom<String, Integer>();
+		hashmap.put("1", 1);
+		hashmap.put("2", 2);
+		hashmap.put("3", 3);
+
+		hashmap.display();
+
+		System.out.println("value corresponding to key 21=" + hashMapCustom.get(21));
+		System.out.println("value corresponding to key 51=" + hashMapCustom.get(51));
+
+		System.out.print("Displaying : ");
+		hashMapCustom.display();
+
+		System.out.println("\n\nvalue corresponding to key 21 removed: " + hashMapCustom.remove(21));
+		System.out.println("value corresponding to key 51 removed: " + hashMapCustom.remove(51));
+
+		System.out.print("Displaying : ");
+		hashMapCustom.display();
+
+	}
+}
