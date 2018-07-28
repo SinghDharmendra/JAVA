2c1533f5c9e925667a6a8f5249eca90b8c80b9d8
Dharmendra Singh <javaprofessional15@gmail.com>
2018-07-28 22:41:51 +0530

    fwritten

--- codebase/Problem-Solving/src/main/java/ps/common/problem/ReverseWordsInString.java
+++ codebase/Problem-Solving/src/main/java/ps/common/problem/ReverseWordsInString.java
@@ -1,26 +1,24 @@
 package ps.common.problem;
 
-import java.util.Arrays;
-
 public class ReverseWordsInString {
 	public static void main(String[] args) {
-		String input="i like this program very much";
-		
-		String out="much very program this like i"; 
-		String ar[]=input.split(" ");
-		int i,j;
-		
-		for(i=0,j=ar.length-1;i<j;i++,j--) {
-			String temp=ar[i];
-			ar[i]=ar[j];
-			ar[j]=temp;
-			
-			
+		String input = "i like this program very much";
+
+		// String out="much very program this like i";
+		String ar[] = input.split(" ");
+		int i, j;
+
+		for (i = 0, j = ar.length - 1; i < j; i++, j--) {
+			String temp = ar[i];
+			ar[i] = ar[j];
+			ar[j] = temp;
+
 		}
-		
-		for(String in:ar)
-			System.out.print(in +" ");
-		
+
+		for (String in : ar)
+			System.out.print(in + " ");
+		System.out.println("Process Completed");
+
 	}
 
 }
