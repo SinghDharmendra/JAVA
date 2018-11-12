cb3975c767b0fcaf757d24dd08dd6409fae51737
Dharmendra Singh <javaprofessional15@gmail.com>
2018-11-12 22:56:55 +0530

    Upgrading to JDK 11
    Change-Id: If2d2e241d4cdeb7b95fedef583cab9ebac1d64ad

--- codebase/MongoDB/src/main/java/mongo/dao/MongoConnection.java
+++ codebase/MongoDB/src/main/java/mongo/dao/MongoConnection.java
@@ -8,7 +8,9 @@ import com.mongodb.MongoClient;
 @RequestScoped
 public class MongoConnection {
 
-	private MongoClient mongoClient = new MongoClient("localhost", 27017);
+	private MongoClient mongoClient = new MongoClient("192.168.1.3", 27017);
+	
+	
 	private DB connection = mongoClient.getDB("tds");
 
 	public DB getConnection() {
